ee0b6542bc20e2c347f49cba1cf514c7c8e7d8f6
Kevin Denk <denk.kevin@web.de>
2017-12-06 17:38:35 +0100

    basic structure of backend

---
+++ server/src/main/java/com/smartshelf/model/Box.java
@@ -0,0 +1,10 @@
+package com.smartshelf.model;
+
+public class Box {
+
+	public long id; 
+	
+	public Item item; 
+	
+	public int amount; 
+}
---
+++ server/src/main/java/com/smartshelf/model/Item.java
@@ -0,0 +1,12 @@
+package com.smartshelf.model;
+
+public class Item {
+
+	public long id; 
+	
+	public String name; 
+	
+	public String description; 
+	
+	public int amount; 
+}
